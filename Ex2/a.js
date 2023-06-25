c966e5be5b511425effdf5660ac286b01d853f73
Lolo <gogapiro@gmail.com>
2023-06-25 17:01:04 +0200

    Second Exercise done

---
+++ Ex2/a.js
@@ -0,0 +1,13 @@
+function listGifts(letter) {
+
+    let obj = {}
+
+    letter.trim().split(' ').filter(pal => !pal.includes('_')).map(pal => !obj[pal] ? obj[pal] = 1 : obj[pal]++)
+
+    return obj
+    
+}
+
+const carta = 'bici coche balón _playstation bici coche peluche'
+const regalos = listGifts(carta)
+console.log(regalos)
