8eb79c2c61f881f601c591ca9c2bd64e2c6dbe7a
loloTheMagician <elperiplodelolo@gmail.com>
2024-03-02 22:30:59 +0100

    Hecho el primer ejercicio, con dos versiones

---
+++ Ejercicio_1/Soluciones/estandard.js
@@ -0,0 +1,22 @@
+import ovejas from "../data.js";
+
+const contarOvejas = (ovejas) => {
+
+    const validColor = 'rojo'
+    const letters = ['a', 'n']
+    const ovejasValidas = []
+
+    for (const oveja of ovejas) {
+        const { color } = oveja
+        if (color !== validColor) continue
+        const { name } = oveja
+        const nameLowerCase = name.toLowerCase()
+        const hasAllLetters = letters.every(letter => nameLowerCase.includes(letter))
+        if (hasAllLetters) ovejasValidas.push(oveja)
+    }
+
+    return ovejasValidas
+
+}
+
+console.log(contarOvejas(ovejas))
---
+++ Ejercicio_1/Soluciones/index.js
@@ -0,0 +1,13 @@
+import ovejas from "../data.js"
+
+const contarOvejas = (ovejas) => {
+    const validColor = 'rojo'
+    const lettersContain = ['a', 'n']
+    return ovejas.filter(({ name, color }) =>
+        color === validColor
+        &&
+        lettersContain.every(letter => name.toLowerCase().includes(letter))
+    )
+}
+
+console.log(contarOvejas(ovejas))
